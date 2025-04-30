4a41bc2e46f701cdd2b96919581f953d1ca62d43
Eugene Francis Rebong <eugene999.dev@gmail.com>
2025-04-30 14:21:37 +0800

    yeah

--- cerberus/run.sh
+++ cerberus/run.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=$(dirname "$0")
 
 open_terminal_and_run() {
     local session_id="$1"
-    open -a Terminal "./$SCRIPT_DIR/run_$session_id.sh" &
+    open -a Terminal "./$SCRIPT_DIR/run_$session_id.sh" & # this is mac only I think
 }
 
 --- Run for sessions 1 to 3 ---
